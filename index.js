6d537af020da253592d98514673e6674377b1725
Nicolás <108936163+nicolasamoroso@users.noreply.github.com>
2022-08-19 11:58:10 -0300

    terminado ahora si

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let  totalventas = 0;
+let  totalcompras = 0;
 
 function agregar() {
     const descripcion = document.getElementById("Descripcion").value;
@@ -14,15 +16,20 @@ function agregar() {
     
         const subtotalInt = parseInt(subtotal)
     
-    
         const transaccion = compra.checked ? compra.value : venta.value;
     
+        if (transaccion === compra.value) {
+            totalcompras += subtotalInt;
+        }
+        else {
+            totalventas += subtotalInt;
+        }
+
         const iva = basico.checked ? 0.22 : minimo.checked ? 0.10 : 0;
     
-        let iva2 = Math.trunc(subtotalInt * iva);
+        let iva2 = subtotalInt * iva;
     
-        let total = Math.trunc(subtotalInt + iva2);
-    
+        let total = Math.round(subtotalInt + iva2);
     
         let append = `
             <tr>
@@ -36,7 +43,22 @@ function agregar() {
         document.getElementById("t-body").innerHTML += append;
     }
     else alert("Debe ingresar una descripcion y un subtotal")
-    
+
     document.getElementById("formulario").reset();
 
+    total(totalventas, totalcompras);
+
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    total(totalventas, totalcompras);
+})
+
+function total(ventas, compras) {
+    let totalC_V = `
+    <label class="mr">Total Ventas $${ventas}</label>
+    <label>Total Compras $${compras}</label>
+    `
+    document.getElementById("total-todo").innerHTML = totalC_V;
+}
+
